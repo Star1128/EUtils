69da7a5743b010865a8224194d797fdc34bdd0a0
Ethan <88174774+Star1128@users.noreply.github.com>
2023-03-12 22:13:14 +0800

    feat: ELog 调整默认 TAG 为调用者类名

--- EUtils/src/main/java/com/ethan/utils/log/ELog.java
+++ EUtils/src/main/java/com/ethan/utils/log/ELog.java
@@ -1,9 +1,12 @@
 package com.ethan.utils.log;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
- * 日志过滤器 在Application的onCreate()中init可以自定义 TAG
+ * 日志过滤器
+ * 在 Application 的 onCreate() 中可以初始化
+ * 不初始化则 TAG 默认为调用者类名
  *
  * @author Ethan 2022/6/10
  */
@@ -14,12 +17,17 @@ public class ELog {
     private static final int INFO = 3;
     private static final int WARN = 4;
     private static final int ERROR = 5;
+    private static final int SHUTDOWN = Integer.MAX_VALUE;
     private static int level;
-    private static String TAG = "Ethan";
+    private static String TAG = "";
 
     public static void init(String tag) {
         TAG = tag;
         level = VERBOSE;
+    }
+
+    public static void init(int lev) {
+        level = lev;
     }
 
     public static void init(String tag, int lev) {
@@ -27,31 +35,46 @@ public class ELog {
         level = lev;
     }
 
+    public static void setTag(String tag) {
+        ELog.TAG = tag;
+    }
+
+    public static void setLevel(int lev) {
+        level = lev;
+    }
+
     public static void v(String msg) {
+        if (TextUtils.isEmpty(TAG)) {
+            TAG = new Exception().getStackTrace()[1].getClassName();
+        }
         if (level <= VERBOSE) {
             Log.v(TAG, msg);
         }
     }
 
     public static void d(String msg) {
+        TAG = new Exception().getStackTrace()[1].getClassName();
         if (level <= DEBUG) {
             Log.d(TAG, msg);
         }
     }
 
     public static void i(String msg) {
+        TAG = new Exception().getStackTrace()[1].getClassName();
         if (level <= INFO) {
             Log.i(TAG, msg);
         }
     }
 
     public static void w(String msg) {
+        TAG = new Exception().getStackTrace()[1].getClassName();
         if (level <= WARN) {
             Log.w(TAG, msg);
         }
     }
 
     public static void e(String msg) {
+        TAG = new Exception().getStackTrace()[1].getClassName();
         if (level <= ERROR) {
             Log.e(TAG, msg);
         }
